ef42181e226ad6b33614a34bd9ddff061f5b9e61
Sho Emoto <sho.emoto@gmail.com>
2020-12-26 19:09:18 +0900

    プロジェクト追加

---
+++ tutorial_Combine_SwiftUI/tutorial_Combine_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorial_Combine_SwiftUI
+//
+//  Created by 江本匠 on 2020/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tutorial_Combine_SwiftUI/tutorial_Combine_SwiftUI/tutorial_Combine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorial_Combine_SwiftUIApp.swift
+//  tutorial_Combine_SwiftUI
+//
+//  Created by 江本匠 on 2020/12/26.
+//
+
+import SwiftUI
+
+@main
+struct tutorial_Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
